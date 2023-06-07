9b918077038fad3ce98c3c6ec44dccfe5cf4c635
miguelzpresa <mikezpresa@gmail.com>
2023-06-07 10:12:52 -0600

    upl

--- tests/opfile.py
+++ tests/opfile.py
@@ -1,4 +1,4 @@
-#!/home/sistemas/kivy_venv/bin/python
+#!/usr/bin/python3
 import kivy
 import kivymd 
 from kivymd.app import MDApp
@@ -13,8 +13,25 @@ from kivy.uix.filechooser import FileChooserListView
 
 Window.size = (350,580)
 
-class LoginScreen(Screen):
-    pass
+class FileChooserScreen(Screen):
+    file_chooser = ObjectProperty(None)
+
+    def select_file(self, path, filename):
+        # Lógica para manejar la selección del archivo
+        print("Archivo seleccionado:", filename[0])
+
+    def show_file_chooser(self):
+        content = FileChooserScreen()
+        popup = Popup(title="Seleccionar archivo", content=content, size_hint=(0.9, 0.9))
+        popup.open()
+
+class MainScreen(Screen):
+    file_chooser_button = ObjectProperty(None)
+
+    def show_file_chooser(self):
+        file_chooser_screen = FileChooserScreen()
+        popup = Popup(title="Seleccionar archivo", content=file_chooser_screen, size_hint=(0.9, 0.9))
+        popup.open()    i
 
 
 class HomeScreen(Screen):
@@ -23,15 +40,12 @@ class HomeScreen(Screen):
 
 class MyApp(MDApp):
     def build(self):
-	
-        self.manager = ScreenManager(transition = NoTransition())
-        self.manager.add_widget(Builder.load_file("opfile.kv"))
+		screen_manager = ScreenManager()
+		screen_manager.add_widget(Builder.load_file('main.kv'))
+		screen_manager.add_widget(Builder.load_file('upload.kv'))
+		screen_manager.add_widget(Builder.load_file('home.kv'))
+		return screen_manager	
 
-        self.manager.add_widget(LoginScreen(name='login'))  # <-- Correct indentation
-        self.manager.add_widget(HomeScreen(name='home'))
-        return self.manager
-    def open_f(self):
-   		pass
 
 
 if __name__ == '__main__':
